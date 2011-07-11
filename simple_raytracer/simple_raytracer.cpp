1f8005ddb34f584d299e90d7f520ee6434e20ae3
Alexander Kondratskiy <g9sash@cdf.toronto.edu>
2011-07-10 22:47:08 -0400

    Committing simple raytracer to take reference to buffer

--- simple_raytracer/simple_raytracer.cpp
+++ simple_raytracer/simple_raytracer.cpp
@@ -1,8 +1,10 @@
 #include "simple_raytracer.h"
+#include "buffer.h"
+
 #include "Eigen/Core"
 #include "Eigen/LU"
 using Eigen::Matrix3d;
-typedef Eigen::Array<RGBColor, Eigen::Dynamic, Eigen::Dynamic> Image;
+//typedef Eigen::Array<RGBColor, Eigen::Dynamic, Eigen::Dynamic> Image;
 
 namespace {
 
@@ -17,17 +19,17 @@ namespace {
 }
 
 // Camera operates as if it is -1 in the z direction facing to +1 in z direction.
-void SimpleRaytracer::render(int width, int height, int fov, Coord& cam) {
-	Image screen(width, height);
+void SimpleRaytracer::render(Buffer& buffer, int fov, Coord& cam) {
+
 
 	Matrix3d w2c = world2cam(cam);
 	Matrix3d cam2world = w2c.inverse();
-	double scale_factor = 1.0 / height;
-	for (int i = 0; i < width; i++) {
-		for(int j = 0; j < height; j++) {
+	double scale_factor = 1.0 / buffer.height();
+	for (int i = 0; i < buffer.width(); i++) {
+		for(int j = 0; j < buffer.height(); j++) {
 			Vector3d cam_ray_dir = Vector3d(i * scale_factor, j * scale_factor, 1);
 			Ray ray = {cam, cam2world * cam_ray_dir};
-			screen(i,j) = scene_.cast_ray(ray);
+			buffer.setPixel(i,j, scene_.cast_ray(ray));
 		}
 	}
 
